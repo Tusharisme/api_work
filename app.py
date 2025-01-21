6dc4752d8ac57b1e3b3b7f0112e1a98ae8844073
Tusharisme <23f2003751@ds.study.iitm.ac.in>
2025-01-21 10:48:07 +0530

    second  commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # Import CORS
+import json
+
+app = Flask(__name__)
+
+CORS(app)
+
+with open("q-vercel-python.json", "r") as file:
+    marks_data = json.load(file)
+
+
+@app.route("/api", methods=["GET"])
+def get_marks():
+    names = request.args.getlist("name")
+
+    result = {"marks": []}
+
+    for name in names:
+        matching_entry = next(
+            (item for item in marks_data if item["name"] == name), None
+        )
+        if matching_entry:
+            result["marks"].append(matching_entry["marks"])
+        else:
+            result["marks"].append(None)
+    return jsonify(result)
+
+
+if __name__ == "_main_":
+    app.run(debug=True)
